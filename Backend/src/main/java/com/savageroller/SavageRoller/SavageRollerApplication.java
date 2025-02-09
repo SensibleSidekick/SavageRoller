e5810697ebc8eca4b28cd2b9f94c50a542d4c414
Kelly Doherty <kad2321@yahoo.com>
2025-02-09 12:56:02 -0500

    Backend commit

---
+++ Backend/src/main/java/com/savageroller/SavageRoller/SavageRollerApplication.java
@@ -0,0 +1,13 @@
+package com.savageroller.SavageRoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavageRollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavageRollerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/savageroller/SavageRoller/SavageRollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savageroller.SavageRoller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavageRollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
